0483d788636a0f78fc131c191b3cd6539aef34bd
yangyuelin <joe14030@hotmail.com>
2018-08-21 17:30:16 +0800

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -38,6 +38,21 @@ function divClick() {
 }
 
 
+$(function() {
+
+	$('input').data('holder', $('input').attr('placeholder'));
+
+	$('input').focusin(function() {
+		$(this).attr('placeholder', '');
+	});
+	$('input').focusout(function() {
+		$(this).attr('placeholder', $(this).data('holder'));
+	});
+})
+
+
+
+
 
 $(function() {
 	$('#sendOrder').on('click', function(e) {
